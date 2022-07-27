6f2fda3b284464d0cc9f59e659a2cb26225033d4
jmh0434 <80581335+jmh0434@users.noreply.github.com>
2022-07-27 14:45:11 +0900

    게시물 삭제 처리 구현

--- main.js
+++ main.js
@@ -12,20 +12,24 @@ app.get('/page/:pageId', (req, res) => { // 상세보기 페이지 구현
     topic.page(req, res);
 });
 
-app.get('/create', (req, res) => { // 페이지 생성 폼 출력 구현
+app.get('/create', (req, res) => { // 게시물 생성 폼 출력 구현
     topic.create(req, res);
 });
 
-app.post('/create_process', (req, res) => { // 페이지 생성 처리 구현
+app.post('/create_process', (req, res) => { // 게시물 생성 처리 구현
     topic.create_process(req, res);
 });
 
-app.get('/update/:pageId', (req, res) => { // 페이지 수정 폼 구현
+app.get('/update/:pageId', (req, res) => { // 게시물 수정 폼 구현
     topic.update(req, res);
 });
 
-app.post('/update_process', (req, res) => { // 페이지 수정 처리 구현
+app.post('/update_process', (req, res) => { // 게시물 수정 처리 구현
     topic.update_process(req, res);
+});
+
+app.post('/delete_process', (req, res) => { // 게시물 삭제 처리 구현
+    topic.delete_process(req, res);
 });
 
 app.listen(port, () => {
